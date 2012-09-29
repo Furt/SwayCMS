d86c118a530983282fd8d4b624ea1d4b0704219f
Furt <lit_69@hotmail.com>
2012-09-29 01:31:02 -0500

    Added placeholders for the upcoming minecraft theme and plugin

---
+++ templates/minecraft/theme.config.php
@@ -0,0 +1,20 @@
+<?php
+/**************************************
+ ** Theme Settings
+ **
+ ** Do not touch these settings unless
+ ** you know what you are doing
+ ** 0=disable 1=enable
+ **************************************/
+
+$theme['topbar']	= '0';
+$theme['motd']		= '0';
+$theme['topnav']	= '0';
+$theme['left_mod']	= '0';
+$theme['right_mod'] = '0';
+$theme['author']	= 'Furt';
+$theme['name']		= 'Minecraft';
+$theme['version']	= '0.1';
+
+
+?>
