c6934b789f62911be28cff7f01297767a1e73819
Furt <lit_69@hotmail.com>
2019-09-24 02:25:25 -0500

    Update swaycms.sql

--- install/swaycms.sql
+++ install/swaycms.sql
@@ -1,3 +1,22 @@
+/*
+SQLyog Community v13.1.5  (64 bit)
+MySQL - 10.1.37-MariaDB : Database - sway
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sway` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `sway`;
+
+/*Table structure for table `menu` */
 
 DROP TABLE IF EXISTS `menu`;
 
@@ -6,11 +25,14 @@ CREATE TABLE `menu` (
   `name` varchar(150) NOT NULL,
   `page` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `menu` */
 
-insert  into `menu`(`id`,`name`,`page`) values (1,'Home',''),(2,'Register','?page=register'),(3,'Help','?page=help');
+insert  into `menu`(`id`,`name`,`page`) values 
+(1,'Home',''),
+(2,'Register','?page=register'),
+(3,'Help','?page=help');
 
 /*Table structure for table `module` */
 
@@ -24,11 +46,14 @@ CREATE TABLE `module` (
   `m_alignment` varchar(6) DEFAULT NULL,
   `m_link` varchar(75) DEFAULT NULL,
   PRIMARY KEY (`m_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `module` */
 
-insert  into `module`(`m_id`,`m_name`,`m_enabled`,`m_position`,`m_alignment`,`m_link`) values (1,'Account',1,1,'right','modules/account.php'),(2,'Navigation',1,1,'left','modules/navigation.php'),(3,'Visitors',1,2,'left','modules/visitors.php');
+insert  into `module`(`m_id`,`m_name`,`m_enabled`,`m_position`,`m_alignment`,`m_link`) values 
+(1,'Account',1,1,'right','modules/account.php'),
+(2,'Navigation',1,1,'left','modules/navigation.php'),
+(3,'Visitors',1,2,'left','modules/visitors.php');
 
 /*Table structure for table `motd` */
 
@@ -38,7 +63,9 @@ CREATE TABLE `motd` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(150) DEFAULT 'Empty',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `motd` */
 
 /*Table structure for table `news` */
 
@@ -51,11 +78,12 @@ CREATE TABLE `news` (
   `title` varchar(50) DEFAULT NULL,
   `news` blob,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 /*Data for the table `news` */
 
-insert  into `news`(`id`,`postdate`,`author`,`title`,`news`) values (1,'01/31/2010','Furt','SwayCMS','SwayCMS is a php content management system designed use with emulators such as : TrinityCore, MaNGOS, ArcEMU, OTserv, and AionUnique. Even if u dont want to use it with a emulator the core has a built in account system so the CMS can be independantly ran.<br />\r\n<br />\r\nWe are currently in alpha stages of development and changes are constant.');
+insert  into `news`(`id`,`postdate`,`author`,`title`,`news`) values 
+(1,'01/31/2010','Furt','SwayCMS','SwayCMS is a php content management system designed use with emulators such as : TrinityCore, MaNGOS, ArcEMU. Even if u dont want to use it with a emulator the core has a built in account system so the CMS can be independantly ran.<br />\r\n<br />\r\nWe are currently in alpha stages of development and changes are constant.');
 
 /*Table structure for table `serverlist` */
 
@@ -69,9 +97,10 @@ CREATE TABLE `serverlist` (
   `mysql_pass` varchar(50) NOT NULL,
   `character_db` varchar(50) NOT NULL,
   `world_db` varchar(50) NOT NULL,
-  `player_cap` tinyint(11) NOT NULL,
+  `auth_db` varchar(50) NOT NULL,
+  `player_cap` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 /*Data for the table `serverlist` */
 
@@ -86,9 +115,11 @@ CREATE TABLE `staff` (
   `name` varchar(24) NOT NULL,
   `location` varchar(24) NOT NULL,
   `age` tinyint(3) NOT NULL,
-  `information` text,
+  `information` text CHARACTER SET latin1,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `staff` */
 
 /*Table structure for table `user_extend` */
 
@@ -100,7 +131,9 @@ CREATE TABLE `user_extend` (
   `avatar` varchar(75) NOT NULL,
   `language` varchar(5) NOT NULL,
   `theme` varchar(10) NOT NULL DEFAULT 'sway'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_extend` */
 
 /*Table structure for table `user_online` */
 
@@ -112,7 +145,9 @@ CREATE TABLE `user_online` (
   `timestamp` varchar(15) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_online` */
 
 /*Table structure for table `users` */
 
@@ -125,5 +160,11 @@ CREATE TABLE `users` (
   `admin` tinyint(5) NOT NULL DEFAULT '0',
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
