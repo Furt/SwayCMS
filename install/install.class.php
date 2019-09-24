000a72d739efdb710488a0e6fd876869067969da
Furt <lit_69@hotmail.com>
2019-09-24 03:04:07 -0500

    Updated installer
    Should be fixed. maybe?

--- install/install.class.php
+++ install/install.class.php
@@ -66,10 +66,10 @@ class Install {
 		return $s;
 	}
 
-	static function executeSqlFile($sql) {
+	static function executeSqlFile($con, $sql) {
 		$query = explode( ";\r", $sql );
 		for( $i=0; $i<count($query);$i++) {
-			if( !mysqli_query( $query[$i] ) ) {
+			if( !mysqli_query( $con, $query[$i] ) ) {
 				return true;
 			}
 		}
@@ -109,7 +109,7 @@ class Install {
 				$site_pass = $_POST['site_pass'];
 				$site_db   = $_POST['site_db'];
 
-				$con       = @mysqli_connect( $site_host, $site_user, $site_pass );
+				$con = @mysqli_connect( $site_host, $site_user, $site_pass );
 
 				if ( mysqli_connect_error() ) {
 					self::showMessage( "Could not open a connection to the mysql service<br />Please double check the information you provided..", "red" );
@@ -120,7 +120,7 @@ class Install {
 				$db = @mysqli_select_db( $site_db, $con );
 
 				if ( mysqli_connect_error() ) {
-					mysqli_close( $con );
+					mysqli_close($con);
 					self::showMessage( "Could not open a connection to the database<br />Please double check the information you provided..", "red" );
 					self::redirect( "index.php?step=$step", 2 );
 					return;
@@ -128,13 +128,13 @@ class Install {
 
 				self::showMessage( "Successfully opened a connection to the mysql service.", "green" );
 				$sql = file_get_contents('swaycms.sql');
-				if(self::executeSqlFile($sql) == true) {
+				if(self::executeSqlFile($con, $sql) == true) {
 					self::showMessage( "<br />Successfully created necessary tables.", "green" );
 					self::redirect( "index.php?step=$next", 2 );
 					return;
 				}
 				else {
-					mysqli_close( $con );
+					mysqli_close($con);
 					self::showMessage( "<br />Failed to create necessary tables.. mysql returned the following: " . mysql_error(), "red" );
 					return;
 				}
@@ -167,113 +167,122 @@ class Install {
 			self::inputCreate( 'Username', 'text', 'accounts_user' );
 			self::inputCreate( 'Password', 'password', 'accounts_pass' );
 			self::inputCreate( 'Database', 'text', 'accounts_db' );
-	  self::optionCreate('Database Structure', 'accounts_core', $cores = array('default', 'mangos'));
-	  self::inputCreate( 'Number of Realms', 'text', 'totalrealms', 0 );
+			self::optionCreate('Database Structure', 'accounts_core', $cores = array('default', 'mangos'));
+			self::inputCreate( 'Number of Realms', 'text', 'totalrealms', 0 );
 
-	  echo '</table><input type=\'submit\' name=\'submit\' value=\'Continue\'></form><br>';
-	  return;
+			echo '</table><input type=\'submit\' name=\'submit\' value=\'Continue\'></form><br>';
+			return;
 		}
 
 		//Step 4
 		if ( $step == 4 ) {
 			$realmid = 0;
 			 
-	  if($_SESSION['totalrealms'] == 0) {
-	  	self::showMessage( "<br />No realm count skipping realm data.", "green" );
-		  $error = true;
-		  self::redirect( "index.php?step=$next", 2 );
-	  } else {
+			if($_SESSION['totalrealms'] == 0) {
+				self::showMessage( "<br />No realm count skipping realm data.", "green" );
+				$error = true;
+				self::redirect( "index.php?step=$next", 2 );
+			} else {
 
-	  	if ( isset( $_POST ) && isset( $_POST['realmid'] ) ) {
-	  		$realmid = $_POST['realmid'];
-	  		unset( $_SESSION['submit'] );
-	  		unset( $_SESSION['realmid'] );
+			if ( isset( $_POST ) && isset( $_POST['realmid'] ) ) {
+				$realmid = $_POST['realmid'];
+				unset( $_SESSION['submit'] );
+				unset( $_SESSION['realmid'] );
 
-	  		foreach ( $_POST as $k => $v ) {
-	  			$_SESSION[$k . "_" . $realmid] = $v;
-	  			unset( $_SESSION[$k] );
-	  		}
+				foreach ( $_POST as $k => $v ) {
+					$_SESSION[$k . "_" . $realmid] = $v;
+					unset( $_SESSION[$k] );
+				}
 
-	  		$realmid += 1;
-	  		if ( $realmid >= $_SESSION['totalrealms'] ) {
-	  			self::redirect( "index.php?step=$next", 0 );
-	  			return;
-	  		}
-	  	}
+				$realmid += 1;
+				if ( $realmid >= $_SESSION['totalrealms'] ) {
+					self::redirect( "index.php?step=$next", 0 );
+					return;
+				}
+			}
 
-	  	echo '<b>Character\'s ' . ( $realmid + 1 ) . ' Database Connection information</b>';
-	  	echo '<table><form method=\'post\' action=\'?step=4\'>';
+				echo '<b>Character\'s ' . ( $realmid + 1 ) . ' Database Connection information</b>';
+				echo '<table><form method=\'post\' action=\'?step=4\'>';
 
-	  	self::inputCreate( 'Realm Name', 'text', 'characters_name' );
-	  	self::inputCreate( 'Hostname', 'text', 'characters_host' );
-	  	self::inputCreate( 'Username', 'text', 'characters_user' );
-	  	self::inputCreate( 'Password', 'password', 'characters_pass' );
-	  	self::inputCreate( 'Database', 'text', 'characters_db' );
-	  	self::optionCreate('Database Structure', 'characters_core',  $cores = array('default', 'mangos'));
-	  	self::inputCreate( 'Max Player Count', 'text', 'characters_pcount', 0 );
-	  	self::inputCreate( '', 'hidden', 'realmid', $realmid );
+				self::inputCreate( 'Realm Name', 'text', 'characters_name' );
+				self::inputCreate( 'Hostname', 'text', 'characters_host' );
+				self::inputCreate( 'Username', 'text', 'characters_user' );
+				self::inputCreate( 'Password', 'password', 'characters_pass' );
+				self::inputCreate( 'Database', 'text', 'characters_db' );
+				self::optionCreate('Database Structure', 'characters_core',  $cores = array('default', 'mangos'));
+				self::inputCreate( 'Max Player Count', 'text', 'characters_pcount', 0 );
+				self::inputCreate( '', 'hidden', 'realmid', $realmid );
 
-	  	echo '</table><input type=\'submit\' name=\'submit\' value=\'Continue\'></form><br>';
-	  	return;
-	  }}
+				echo '</table><input type=\'submit\' name=\'submit\' value=\'Continue\'></form><br>';
+				return;
+			}
+		}
 
-	  //Step 5
-	  if ( $step == 5 ) {
-	  	$error = true;
+		//Step 5 needs fixed
+		if ( $step == 5 ) {
+			$error = true;
 
-	  	if ( isset( $_POST ) && isset( $_POST['sitename'] ) ) {
-	  		self::redirect( "index.php?step=$next", 0 );
-	  		return;
-	  	}
+			if ( isset( $_POST ) && isset( $_POST['sitename'] ) ) {
+				self::redirect( "index.php?step=$next", 0 );
+				return;
+			}
 
-	  	echo '<h1>Site Information</h1>';
-	  	echo '<table><form method=\'post\' action=\'?step=5\'>';
+			echo '<h1>Site Information</h1>';
+			echo '<table><form method=\'post\' action=\'?step=5\'>';
 
-	  	self::inputCreate( 'Site Name', 'text', 'sitename' );
-	  	self::optionCreate('News Plugin', 'news_plugin', $news = array('default'));
-	  	self::inputCreate( 'News Posts', 'text', 'news_post' );
-	  	self::optionCreate('Theme', "theme", $themes = array('default', 'sway'));
-	  	self::optionCreate('Language', 'language', $lang = array('english'));
-	  	self::inputCreate( 'Forum Link', 'text', 'forum_link' );
+			self::inputCreate( 'Site Name', 'text', 'sitename' );
+			self::optionCreate('News Plugin', 'news_plugin', $news = array('default'));
+			self::inputCreate( 'News Posts', 'text', 'news_post' );
+			self::optionCreate('Emulator', "emu", $themes = array('default', 'mangos', 'trinitycore'));
+			self::optionCreate('Theme', "theme", $themes = array('default', 'sway'));
+			self::optionCreate('Language', 'language', $lang = array('english'));
+			self::inputCreate( 'Forum Link', 'text', 'forum_link' );
 
-	  	echo '</table><input type=\'submit\' name=\'submit\' value=\'Continue\'></form><br>';
-	  }
+			echo '</table><input type=\'submit\' name=\'submit\' value=\'Continue\'></form><br>';
+		}
 
-	  if ( $step == 6 ) {
-	  	$confedit = new ConfigEditor();
-	  	if(file_exists('../includes/config.inc.php')) {
-	  		$confedit->LoadFromFile('../includes/config.inc.php');
-	  	}
-	  	$confedit->SetVar('site', array('name'		=> $_SESSION['sitename'],
-										'template'	=> $_SESSION['theme'],
-										'news'		=> $_SESSION['news_plugin'],
-										'news_post'	=> $_SESSION['news_post'],
-										'plugin'	=> $_SESSION['accounts_core'],
-										'language'	=> $_SESSION['language'],
-										'forum'		=> $_SESSION['forum_link']
-	  	), 'Site Options');
+		if ( $step == 6 ) {
+			$confedit = new ConfigEditor();
+			if(file_exists('../includes/config.inc.php')) {
+				$confedit->LoadFromFile('../includes/config.inc.php');
+			}
+			$confedit->SetVar('site', array('name'		=> $_SESSION['sitename'],
+											'template'	=> $_SESSION['theme'],
+											'news'		=> $_SESSION['news_plugin'],
+											'news_post'	=> $_SESSION['news_post'],
+											'plugin'	=> $_SESSION['emu'],
+											'language'	=> $_SESSION['language'],
+											'forum'		=> $_SESSION['forum_link']), 'Site Options');
 
-	  	$confedit->SetVar('website', array('host' => $_SESSION['site_host'], 'user' => $_SESSION['site_user'], 'pass' => $_SESSION['site_pass'], 'database' => $_SESSION['site_db']), 'Website database');
-	  	$confedit->SetVar('account', array('host' => $_SESSION['accounts_host'], 'user' => $_SESSION['accounts_user'], 'pass' => $_SESSION['accounts_pass'], 'database' => $_SESSION['accounts_db']), 'Account database');
-	  	$config_php = $confedit->Save('../includes/config.inc.php');
-	  	@chmod("../config.php", 0644);
-	  	if ( is_writable( "../includes/config.inc.php" ) ) {
-	  		self::showMessage( "The config.php file no longer needs to be chmod 0777, we recommend you change it to 0644 to prevent others from editting the file.", "red" );
-	  	}
-	  	self::redirect( "index.php?step=$next", 2 );
-	  	return;
-	  }
+			$confedit->SetVar('website', array(	'host' 		=> $_SESSION['site_host'],
+												'user' 		=> $_SESSION['site_user'],
+												'pass' 		=> $_SESSION['site_pass'],
+												'database' 	=> $_SESSION['site_db']), 'Website database');
+												
+			$confedit->SetVar('account', array( 'host' => $_SESSION['accounts_host'],
+												'user' => $_SESSION['accounts_user'],
+												'pass' => $_SESSION['accounts_pass'],
+												'database' => $_SESSION['accounts_db']), 'Account database');
+												
+			$config_php = $confedit->Save('../includes/config.inc.php');
+			@chmod("../config.php", 0644);
+			if ( is_writable( "../includes/config.inc.php" ) ) {
+				self::showMessage( "The config.php file no longer needs to be chmod 0777, we recommend you change it to 0644 to prevent others from editting the file.", "red" );
+			}
+			self::redirect( "index.php?step=$next", 2 );
+			return;
+		}
 
-	  //Step 7
-	  if ( $step == '7' ) {
-	  	// Installation complete
-	  	self::showMessage( "Installation is now complete, you will now be redirected to home page of the site.", "blue" );
-	  	self::showMessage( "<br />At this time you should rename or remove /install/index.php for security reasons.", "green" );
-	  	self::redirect( "../index.php", 15 );
-	  	return;
-	  }
+		//Step 7
+		if ( $step == '7' ) {
+			// Installation complete
+			self::showMessage( "Installation is now complete, you will now be redirected to home page of the site.", "blue" );
+			self::showMessage( "<br />At this time you should rename or remove /install/index.php for security reasons.", "green" );
+			self::redirect( "../index.php", 15 );
+			return;
+		}
 
-	  if ( !$error ) { echo '<br><a href=\'?step=' . $next . '\'>You may continue to the next step</a>'; }
+		if ( !$error ) { echo '<br><a href=\'?step=' . $next . '\'>You may continue to the next step</a>'; }
 	}
 }
 ?>
